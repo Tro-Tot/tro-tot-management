1f6736ed03f1ed2c1760515cd8738044bdfa71a6
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-07-30 00:38:07 +0700

    Color constants + Font family set up [KienNDT]

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import './App.css';
+import { HiAdjustments } from 'react-icons/hi';
+import { Button } from './components/ui/button';
+import Colors from './constants/color';
 
 function App() {
   return (
     <>
-      <div className="">Hello world</div>
+      <div className="font-bold text-3xl text-primaryDark">
+        This is the management page
+      </div>
     </>
   );
 }
