7681f8ce91edb8fea5989c249075d1f3e718f981
Nguyen Dinh Trung Kien <devlindinh2003@gmail.com>
2024-07-30 01:06:27 +0700

    Resolve conflict [KienNDT]

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { HiAdjustments } from 'react-icons/hi';
-import { Button } from './components/ui/button';
-import Colors from './constants/color';
-import RouterComponent from '@';
+import { Button } from '@/components/ui/button';
 
 function App() {
   return (
-    <>
-      <div className="font-bold text-3xl text-primaryDark">
-        This is the management page
-      </div>
-      <RouterComponent />
-    </>
+    <div className="App">
+      <span className="text-primaryLight">Settings</span>
+    </div>
   );
 }
-
 export default App;
